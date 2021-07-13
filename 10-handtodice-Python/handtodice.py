161489225ac6e5cf266b4051adbeb286c9de830b
venu912 <venuandalam912@gmail.com>
2021-07-13 09:21:20 +0530

    commit2

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -6,7 +6,7 @@
 # Hint: You might find // and % useful here, and also getKthDigit().
 
 def handtodice(hand):
-  	a=hand%10
+		a=hand%10
 		b=(hand//10)%10
 		c=(hand//100)
-		pass
+		return (c,b,a)
