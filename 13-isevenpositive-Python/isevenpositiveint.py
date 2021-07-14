fbda7f07d5f9043a321a05ebed6ac1a6b4891959
venu912 <venuandalam912@gmail.com>
2021-07-14 11:26:24 +0530

    commit1

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -5,5 +5,9 @@
 # than crashing), and isevenpositiveint(123456) returns True.
 
 def isevenpositiveint(x):
+	if(x>1 and x%2==0):
+			return True
+	else:
+			return False
 	# your code goes here
-	pass
+	
