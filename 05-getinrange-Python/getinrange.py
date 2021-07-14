1972ec24bd09d28e0fe253e2e1d0b761f4d39e11
venu912 <venuandalam912@gmail.com>
2021-07-14 15:44:00 +0530

    commit4

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -2,11 +2,10 @@
 # x, bound1, and bound2, where bound1 is not necessarily less than bound2. 
 # If x is between the two bounds, just return it unmodified. Otherwise, 
 # if x is less than the lower bound, return the lower bound, 
-# or if x is greater than the upper bound, return the upper bound.
-
+# or i
 def fun_getinrange(x, bound1, bound2):
-		if(bound1>x>bound2 or bound1<x<bound2):
-				return x
+		if(bound1>x>bound2) or (bound1<x<bound2):
+  			return x
 		elif(x<bound1):
 			if(bound1>bound2):
 					return bound2
@@ -17,4 +16,4 @@ def fun_getinrange(x, bound1, bound2):
 					return bound1
 			else:
 					return bound2
-	
+  			
