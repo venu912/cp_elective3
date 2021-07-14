6cd3689d0c2e04ae56f4d20bd0aa36dc1372275e
venu912 <venuandalam912@gmail.com>
2021-07-14 10:59:15 +0530

    commit3

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -5,9 +5,7 @@
 # or if x is greater than the upper bound, return the upper bound.
 
 def fun_getinrange(x, bound1, bound2):
-		if(bound1>x>bound2):
-				return x
-		elif(bound1<x<bound2):
+		if(bound1>x>bound2 or bound1<x<bound2):
 				return x
 		elif(x<bound1):
 			if(bound1>bound2):
