823f9df162338d1d77937252fd7a4ecf5d8880d4
venu912 <venuandalam912@gmail.com>
2021-07-16 11:24:08 +0530

    commit2

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -5,8 +5,8 @@
 # if the two circles intersect and False otherwise.
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
-		distance=(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)
-		radius=(r1+r2)*(r1+r2)
+		distance=(x1-x2)**2+(y1-y2)**2
+		radius=(r1+r2)**2
 		if(distance==radius):
 				return True
 		elif (distance > radius):
