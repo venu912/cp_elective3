f19639b0b52ed32a04babfa7865e3ecc3192ea45
venu912 <venuandalam912@gmail.com>
2021-07-22 14:58:42 +0530

    commit3

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -7,39 +7,39 @@
 
 
 def fun_nth_additive_prime(n):
-		found=1
-		guess=1
-		count=0
-		while(found):
-			if(additive_prime(guess)):
-
-				if(n==count):
-					found=0
-					return guess
-
-				guess=guess+1
-				count+=1
-			else:
-				guess=guess+1
+	found = 0
+	guess = 0
+	while (found <= n):
+			guess += 1
+			if (isprime(guess) and additive_prime(guess)):
+					found += 1
+	return guess
 
 def additive_prime(n):
+  	
 		sum=0
 		while(n):
 				m=n%10
 				sum=sum+m
 				n=n//10
-		if(isprime(sum)):
+		if(sum>1 and isprime(sum)):
   			return sum
 
-  	
-
 def isprime(x):
-		if(x==2):
-				return True
-		else:
-				for i in range(2,x//2+1):
-						if(x%i==0):
-								return False
+		if(x<2):
+				return False
+		for i in range(2,x):
+				if(x%i==0):
+						return False
 		return True
+
+#(0,2),(1,3),(5,23),(7,41),(20,157),(25,197)
+# print(fun_nth_additive_prime(7))
+# print(fun_nth_additive_prime(0))
+# print(fun_nth_additive_prime(1))
+# print(fun_nth_additive_prime(25))
+# print(fun_nth_additive_prime(4))
+# print(fun_nth_additive_prime(5))
+
 
 	
