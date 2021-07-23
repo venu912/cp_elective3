49b353a91d7c7126e44a42b538fc7409bb935542
venu912 <venuandalam912@gmail.com>
2021-07-23 21:24:58 +0530

    commit0

--- 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
+++ 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
@@ -10,7 +10,64 @@
 
 
 
-import math
+#import math
 
 def fun_nearestkaprekarnumber(n):
-    return 1
+    if(kaprekarnumber(n)):
+        return n
+    else:
+        a=n
+        for i in range(1,n):
+            if(i%2!=0):
+                a=a-i
+                if(kaprekarnumber(a)):
+                    return a
+            else:
+                a=a+i
+                if(kaprekarnumber(a)):
+                    return a 
+                
+def kaprekarnumber(n):
+    a=n*n
+    y=a
+    count1=0
+    while(a):
+        count1+=1
+        a=a//10
+    count2=count1
+    z=1
+    b=count2//2
+    while(b):
+        z=z*10
+        b-=1
+    if(count1%2!=0):
+        z=z*10    
+    c=y//z
+    d=y%z
+    e=c+d
+    if(n==e):
+        return e
+    else:
+        c=str(c)
+        d=str(d)
+        while(len(c)):
+            if(c[-1]=='0'):
+                c=c[0:-1]
+            else:
+                break
+        while(len(d)):
+            if(d[-1]=='0'):
+                d=d[0:-1]
+            else:
+                break
+        if(len(c)==0 or len(d)==0):
+            return False
+        sum=int(c)+int(d)
+        if(sum==n):
+            return True
+    return False
+
+print(fun_nearestkaprekarnumber(3861))
+print(fun_nearestkaprekarnumber(4878))
+print(kaprekarnumber(4879))
+print(kaprekarnumber(3861))
