e1b518fb5c5d55df2539d0d154782850e37f4524
venu912 <venuandalam912@gmail.com>
2021-07-27 21:05:12 +0530

    commit1

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,30 +8,48 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
-def fun_recursion_onlyevendigits(l): 
-	if(len(l)==0):
-		return 0
-	i=0
-	sum=0
-	a=only_evendigits(l,i,sum)
-	return a
+# def fun_recursion_onlyevendigits(l): 
+# 	if(len(l)==0):
+# 		return 0
+# 	i=0
+# 	sum=0
+# 	a=only_evendigits(l,i,sum)
+# 	return a
 
-def only_evendigits(l,i,sum):
-	if(i<len(l)):
-			i+=1
-			j=0
-			b=only_even(l[i],str(l[i]),j)
+# def only_evendigits(l,i,sum):
+# 	if(i<len(l)):
+# 			i+=1
+# 			j=0
+# 			b=only_even(l[i],str(l[i]),j)
 
-def only_even(p,q,j):
-	if(j<len(q)):
-		j+=1
-		if(p%10==0):
+# def only_even(p,q,j):
+# 	if(j<len(q)):
+# 		j+=1
+# 		if(p%10==0):
   		
-			p=p//10
-			return only_even(p,q,j)
-		elif((p//10)%2==0):
-  			total=total*
-				p=p//10
-				return only_even(p,q,j)
+# 			p=p//10
+# 			return only_even(p,q,j)
+# 		elif((p//10)%2==0):
+#   			total=total*
+# 				p=p//10
+# 				return only_even(p,q,j)
+# 	else:
+# 		return 
+
+def fun_recursion_onlyevendigits(l):
+	empty=[]
+	if(len(l)==0):
+		return empty
 	else:
-		return 
+		even=wrapper(l[0])
+		empty.append(even)
+		return empty+fun_recursion_onlyevendigits(l[1:])
+
+def wrapper(n,i=0,null=0):
+	if(n==0):
+		return null
+	temp=n%10
+	if(temp%2==0):
+		null+=(10**i*temp)
+		i+=1
+	return wrapper(n//10,i,null)
