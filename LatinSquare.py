9de49058f087e05e358915e57adaafcfc688ed55
venu912 <venuandalam912@gmail.com>
2021-08-03 20:41:27 +0530

    commit0

--- LatinSquare.py
+++ LatinSquare.py
@@ -7,6 +7,24 @@
 
 # Write your own test cases...
 
-def isLatinSquare(lst):
-    # Your code goes here...
-    pass
+def isLatinSquare(l):
+    for i in range(len(l)):
+        s=set(l[i])
+        if(len(l[i])==len(s)):
+            if(s==set(l[0])):
+                continue
+            else:
+                return False
+        else:
+            return False
+    for j in range(len(l)):
+        a=[]
+        for i in range(len(l)):
+            a.append(l[i][j])
+        s=set(a)
+        if(len(a)==len(s)):
+            if()
+    return True
+
+print(isLatinSquare([[1,2,3],[2,3,1],[3,1,2]]))    
+    
