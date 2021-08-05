5201cd23bb93a80f5f8de93fcdd1df49708f07f7
venu912 <venuandalam912@gmail.com>
2021-08-05 17:51:19 +0530

    commit1

--- LatinSquare.py
+++ LatinSquare.py
@@ -7,24 +7,39 @@
 
 # Write your own test cases...
 
-def isLatinSquare(l):
-    for i in range(len(l)):
-        s=set(l[i])
-        if(len(l[i])==len(s)):
-            if(s==set(l[0])):
-                continue
-            else:
-                return False
-        else:
-            return False
-    for j in range(len(l)):
-        a=[]
-        for i in range(len(l)):
-            a.append(l[i][j])
-        s=set(a)
-        if(len(a)==len(s)):
-            if()
-    return True
+def isLatinSquare(a):
+    # Your code goes here...
+    N = len(a)
+    rows = []
+    for i in range(N):
+        rows.append(set([]))
+    cols = []
+    for i in range(N):
+        cols.append(set([]))
+    invalid = 0
+    for i in range(N):
+        for j in range(N):
+            rows[i].add(a[i][j])
+            cols[j].add(a[i][j])
+  
+            if (a[i][j] > N or a[i][j] <= 0) :
+                invalid += 1
+    numrows = 0
+    numcols = 0
+  
+    for i in range(N):
+        if (len(rows[i]) != N) :
+            numrows+=1
+        if (len(cols[i]) != N) :
+            numcols+=1
+    if (numcols == 0 or numrows == 0 and invalid == 0) :
+        return "Yes"
+    else:
+        return "No"
 
-print(isLatinSquare([[1,2,3],[2,3,1],[3,1,2]]))    
+
+assert(isLatinSquare([[1,2],[3,1]])=="Yes")
+print("All Passed")
+
+#print(isLatinSquare([[1,2,3],[2,3,1],[3,1,2]]))    
     
