17dd05445e96cae02d783cba41b1c09510f79efd
venu912 <venuandalam912@gmail.com>
2021-08-05 10:58:08 +0530

    commit2

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -57,79 +57,79 @@
 #     return count
 
 
-def smithnumber(n):
-    empty=[]
-    for i in range(2,n):
-        if(isprime(i) and (n%i==0)):
-            empty.append(i)
+# def smithnumber(n):
+#     empty=[]
+#     for i in range(2,n):
+#         if(isprime(i) and (n%i==0)):
+#             empty.append(i)
             
 
-def isprime(n):
-    for i in range(2,n):
-        if(i%2==0):
-            return False
-    return True
+# def isprime(n):
+#     for i in range(2,n):
+#         if(i%2==0):
+#             return False
+#     return True
 
-print(smithnumber(105))
+# print(smithnumber(105))
 #print(fun_nth_smithnumber(17))
 
 
 
 
 
-# def isprime(n):
-#     if n==1:
-#         return False
-#     if n==2:
-#         return True    
-#     for i in range(2, (n//2)+1):
-#         if(n%i==0):
-#             return False
-#     else:
-#         return True
+def isprime(n):
+    if n==1:
+        return False
+    if n==2:
+        return True    
+    for i in range(2, (n//2)+1):
+        if(n%i==0):
+            return False
+    else:
+        return True
        
-# def smith(n):
-#     if(isprime(n)!=True):
-#         k = n
-#         factorslist = []
-#         factorssum=0
-#         digitsum = 0
-#         if n==1:
-#             return False
-#         for i in range(2,(n//2) + 1):
-#             if(isprime(i)==True) and (n%i==0):
-#                 factorslist.append(i)
+def smith(n):
+    if(isprime(n)!=True):
+        k = n
+        factorslist = []
+        factorssum=0
+        digitsum = 0
+        if n==1:
+            return False
+        for i in range(2,(n//2) + 1):
+            if(isprime(i)==True) and (n%i==0):
+                factorslist.append(i)
          
-#         for an in str(n):
-#             digitsum+=int(an)
+        for an in str(n):
+            digitsum+=int(an)
        
-#         li=[]
-#         for i in factorslist:
-#             while (k%i==0 and k!=0):
-#                 li.append(i)    
-#                 factorssum+=i
-#                 k=k//i
+        li=[]
+        for i in factorslist:
+            while (k%i==0 and k!=0):
+                li.append(i)    
+                factorssum+=i
+                k=k//i
       
-#         repeatsuminfactors = 0        
-#         for j in li:
-#             if(j>9):
-#                 for d in str(j):
-#                     repeatsuminfactors+=int(d)
-#             else:
-#                repeatsuminfactors+=j
+        repeatsuminfactors = 0        
+        for j in li:
+            if(j>9):
+                for d in str(j):
+                    repeatsuminfactors+=int(d)
+            else:
+               repeatsuminfactors+=j
         
-#         if(digitsum==repeatsuminfactors):
-#             return True
-#         else:
-#             return False
-# def fun_nth_smithnumber(n):
-#     count=0
-#     position=1
-#     result = 0
-#     while(n+1!=count):
-#         if(smith(position)==True):
-#             count+=1
-#             result = position
-#         position+=1
-#     return result
+        if(digitsum==repeatsuminfactors):
+            return True
+        else:
+            return False
+def fun_nth_smithnumber(n):
+    count=0
+    position=1
+    result = 0
+    while(n+1!=count):
+        if(smith(position)==True):
+            count+=1
+            result = position
+        position+=1
+    return result
 
